6003edd23a3c0cfe0aeea789b311198307296dd6
SachinC2003 <156447637+SachinC2003@users.noreply.github.com>
2024-10-20 13:10:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,16 @@ const teacherRoute = require('./routes/teacher');
 const userRoute = require('./routes/user')
 const adminRoute = require('./routes/admin')
 
+// 1. Update Express CORS setup
 const corsOptions = {
-  origin: '*', 
-  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+  origin: 'https://course-craft-client.vercel.app',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   credentials: true,
+  optionsSuccessStatus: 204
 };
 app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 
 const PORT = 3000;
 
